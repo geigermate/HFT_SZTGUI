f8b3734917cc97d4e1ba842ecf4c7878f591bb10
geigermate <geigermate@stud.uni-obuda.hu>
2022-04-23 09:43:36 +0200

    CustomerRepository added

---
+++ F27T0P_HFT_2021222.Repository/ModelRepositories/CustomerRepository.cs
@@ -0,0 +1,32 @@
+﻿using F27T0P_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F27T0P_HFT_2021222.Repository.ModelRepositories
+{
+    public class CustomerRepository : Repository<Customer>, IRepository<Customer>
+    {
+        public CustomerRepository(GpuCustomerDbContext ctx) : base(ctx)
+        {
+
+        }
+
+        public override Customer Read(int id)
+        {
+            return ctx.Customers.FirstOrDefault(c => c.Id == id);
+        }
+
+        public override void Update(Customer item)
+        {
+            var old = Read(item.Id);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
