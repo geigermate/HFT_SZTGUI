a69b6c2d1e11c95fa2034a309e901a417edc5f32
geigermate <geigermate@stud.uni-obuda.hu>
2022-04-23 19:32:20 +0200

    Test init added

--- F27T0P_HFT_2021222.Test/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace F27T0P_HFT_2021222.Test
-{
-    public class Class1
-    {
-    }
-}
---
+++ F27T0P_HFT_2021222.Test/LogicTester.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using F27T0P_HFT_2021222.Logic;
+using F27T0P_HFT_2021222.Logic.Interfaces;
+using F27T0P_HFT_2021222.Repository.ModelRepositories;
+using F27T0P_HFT_2021222.Repository;
+using F27T0P_HFT_2021222.Models;
+
+namespace F27T0P_HFT_2021222.Test
+{
+    [TestFixture]
+    public class LogicTester
+    {
+        CustomerLogic cl;
+        Mock<IRepository<Customer>> mockCustomerRepo;
+
+        [SetUp]
+        public void Init()
+        {
+            var gpus = new List<GpuType>()
+            {
+                new GpuType()
+                {
+                    Id = 1,
+                    Name = "GTX 1660",
+                    BasePrice = 150000,
+                    CustomerId = 1
+                },
+
+                new GpuType()
+                {
+                    Id = 2,
+                    Name = "RX 6950XT",
+                    BasePrice = 1350000,
+                    CustomerId = 1
+                }
+            };
+
+            Customer fakeCustomer = new Customer()
+            {
+                Id = 1,
+                Name = "Tesztelo",
+                BoughtCards = gpus,
+            };
+
+            mockCustomerRepo = new Mock<IRepository<Customer>>();
+
+            mockCustomerRepo.Setup(xy => xy.ReadAll())
+                            .Returns((IQueryable<Customer>)fakeCustomer);
+
+            cl = new CustomerLogic(mockCustomerRepo.Object);
+        }
+    }
+}
