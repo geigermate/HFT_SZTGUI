45381fac7f167d62a62436a0aaea5d47a2fceced
geigermate <geigermate@stud.uni-obuda.hu>
2022-04-23 09:40:30 +0200

    GpuTypeRepository added

---
+++ F27T0P_HFT_2021222.Repository/ModelRepositories/GpuTypeRepository.cs
@@ -0,0 +1,31 @@
+﻿using F27T0P_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F27T0P_HFT_2021222.Repository.ModelRepositories
+{
+    public class GpuTypeRepository : Repository<GpuType>, IRepository<GpuType>
+    {
+        public GpuTypeRepository(GpuCustomerDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override GpuType Read(int id)
+        {
+            return ctx.GpuTypes.FirstOrDefault(gpu => gpu.Id == id);
+        }
+
+        public override void Update(GpuType item)
+        {
+            var old = Read(item.Id);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
