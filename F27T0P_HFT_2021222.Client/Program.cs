b1c62ebd3e1a32d7d7db2d1b337b077d19bd8746
geigermate <geigermate@stud.uni-obuda.hu>
2022-04-30 20:58:26 +0200

    RestService implemented

--- F27T0P_HFT_2021222.Client/Program.cs
+++ F27T0P_HFT_2021222.Client/Program.cs
@@ -1,14 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Linq;
 
 namespace F27T0P_HFT_2021222.Client
 {
     class Program
     {
+        static RestService rest;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine();
-            Console.WriteLine();
+            rest = new RestService("http://localhost:42137/");
         }
     }
 }
