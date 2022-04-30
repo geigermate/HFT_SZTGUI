ff77c0b5626060fa29c29c183e9aa23e8f443fbb
geigermate <geigermate@stud.uni-obuda.hu>
2022-04-30 16:45:44 +0200

    Startup changes added

--- F27T0P_HFT_2021222.Endpoint/Startup.cs
+++ F27T0P_HFT_2021222.Endpoint/Startup.cs
@@ -1,8 +1,15 @@
+using Castle.Core.Configuration;
+using F27T0P_HFT_2021222.Logic;
+using F27T0P_HFT_2021222.Logic.Interfaces;
+using F27T0P_HFT_2021222.Models;
+using F27T0P_HFT_2021222.Repository;
+using F27T0P_HFT_2021222.Repository.ModelRepositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +19,26 @@ namespace F27T0P_HFT_2021222.Endpoint
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<GpuCustomerDbContext>();
+            services.AddTransient<IRepository<Customer>, CustomerRepository>();
+            services.AddTransient<ICustomerLogic, CustomerLogic>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,9 +47,13 @@ namespace F27T0P_HFT_2021222.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
             }
 
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
